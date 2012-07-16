9586ac29af45331b31cec34e16b3f630f815d777
Mohamed Attahri <mohamed.attahri@greendizer.com>
2012-07-17 00:06:43 +0100

    Improved the setup experience.

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 # -*- coding: utf-8 -*-
-from distutils.core import setup
-from setuptools import find_packages
-
+try:
+    from distutils.core import setup
+except ImportError:
+    from setuptools import setup
 
 setup(
-    name='pyxmli',
-    packages=find_packages(),
-    version='0.0.1',
-    author=u'Greendizer',
-    author_email='developers@greendizer.com',
-    packages=['pyxmli'],
-    install_requires=['pycrypto >= 2.5', 'lxml >= 2.3'],
-    url='http://github.com/Greendizer/PyXMLi',
-    license=open('licence.txt').read(),
-    description='Create and sign XMLi invoices in Python.',
-    long_description=open('README.markdown').read(),
-    zip_safe=True,
+    name                = 'pyxmli',
+    packages            = ['pyxmli'],
+    version             = open('VERSION').read(),
+    author              = u'Greendizer',
+    author_email        = 'support@greendizer.com',
+    packages            = ['pyxmli'],
+    package_data        = {'pyxmli' : ['../VERSION']},
+    install_requires    = ['pycrypto >= 2.5',
+                           'lxml <= 2.3'],
+    url                 = 'http://github.com/Greendizer/PyXMLi',
+    license             = open('LICENCE').read(),
+    description         = 'Create and sign XMLi invoices in Python.',
+    long_description    = open('README.markdown').read(),
+    zip_safe            = True,
+    test_suite          = 'tests'
 )
