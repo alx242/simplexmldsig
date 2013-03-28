c617b473fd4538d8df900d28615672fb03939890
Alexander Schüssler <alex@bonit.(none)>
2013-03-28 15:27:39 +0100

    Cleanup whitespaces and various PEP8 stuff. Allow adding of certificate to signature

--- simplexmldsig.py
+++ simplexmldsig.py
@@ -26,15 +26,15 @@ from Crypto.Util.number import long_to_bytes, bytes_to_long
 
 KEY_INFO_PATH = 'xmldsig:KeyInfo/xmldsig:KeyValue/xmldsig:RSAKeyValue'
 
-NAMESPACES = {'xmli' : 'http://xmli.org',
-              'xmldsig': 'http://www.w3.org/2000/09/xmldsig#',}
+NAMESPACES = {'xmli': 'http://xmli.org',
+              'xmldsig': 'http://www.w3.org/2000/09/xmldsig#'}
 
 PTN_SIGNED_INFO_XML = \
-'<SignedInfo xmlns="http://www.w3.org/2000/09/xmldsig#"><CanonicalizationMethod Algorithm="http://www.w3.org/TR/2001/REC-xml-c14n-20010315#WithComments"></CanonicalizationMethod><SignatureMethod Algorithm="http://www.w3.org/2000/09/xmldsig#rsa-sha1"></SignatureMethod><Reference URI=""><Transforms><Transform Algorithm="http://www.w3.org/2000/09/xmldsig#enveloped-signature"></Transform></Transforms><DigestMethod Algorithm="http://www.w3.org/2000/09/xmldsig#sha1"></DigestMethod><DigestValue>%(digest_value)s</DigestValue></Reference></SignedInfo>'
+    '<SignedInfo xmlns="http://www.w3.org/2000/09/xmldsig#"><CanonicalizationMethod Algorithm="http://www.w3.org/TR/2001/REC-xml-c14n-20010315#WithComments"></CanonicalizationMethod><SignatureMethod Algorithm="http://www.w3.org/2000/09/xmldsig#rsa-sha1"></SignatureMethod><Reference URI=""><Transforms><Transform Algorithm="http://www.w3.org/2000/09/xmldsig#enveloped-signature"></Transform></Transforms><DigestMethod Algorithm="http://www.w3.org/2000/09/xmldsig#sha1"></DigestMethod><DigestValue>%(digest_value)s</DigestValue></Reference></SignedInfo>'
 PTN_SIGNATURE_XML = \
-'<Signature xmlns="http://www.w3.org/2000/09/xmldsig#">%(signed_info_xml)s<SignatureValue>%(signature_value)s</SignatureValue>%(key_info_xml)s</Signature>'
+    '<Signature xmlns="http://www.w3.org/2000/09/xmldsig#">%(signed_info_xml)s<SignatureValue>%(signature_value)s</SignatureValue>%(key_info_xml)s</Signature>'
 PTN_KEY_INFO_RSA_KEY = \
-'<KeyInfo><KeyValue><RSAKeyValue><Modulus>%(modulus)s</Modulus><Exponent>%(exponent)s</Exponent></RSAKeyValue></KeyValue></KeyInfo>'
+    '<KeyInfo><X509Data><X509Certificate>%(x509)s</X509Certificate></X509Data><KeyValue><RSAKeyValue><Modulus>%(modulus)s</Modulus><Exponent>%(exponent)s</Exponent></RSAKeyValue></KeyValue></KeyInfo>'
 
 
 def b64e(s):
@@ -62,7 +62,7 @@ def c14n(xml):
     return c14nized
 
 
-def sign(xml, private, public):
+def sign(xml, private, public, cert):
     '''
     Return xmldsig XML string from xml_string of XML.
     @param xml: str of bytestring xml to sign
@@ -79,10 +79,10 @@ def sign(xml, private, public):
     signature_xml = PTN_SIGNATURE_XML % {
         'signed_info_xml': signed_info_xml,
         'signature_value': binascii.b2a_base64(signature_value)[:-1],
-        'key_info_xml': _generate_key_info_xml_rsa(public.key.n, public.key.e)
+        'key_info_xml': _generate_key_info_xml_rsa(public.key.n, public.key.e, cert)
     }
 
-    position = xml.rfind('</')
+    position = xml.rfind('</s:Security>')
     return xml[0:position] + signature_xml + xml[position:]
 
 
@@ -90,45 +90,45 @@ def verify(xml):
     '''Verifies whether the validity of the signature contained in an XML
     document following the XMLDSig standard'''
     try:
-        from cStringIO import cStringIO as StringIO 
+        from cStringIO import cStringIO as StringIO
     except ImportError:
         from StringIO import StringIO
-    
     xml = xml.encode('utf-8', 'xmlcharrefreplace')
     tree = etree.parse(StringIO(xml))
-    
     try:
         '''Isolating the Signature element'''
-        signature_tree = tree.xpath('xmldsig:Signature',
+        signature_tree = tree.xpath('//xmldsig:Signature',
                                     namespaces=NAMESPACES)[0]
-    
-        '''Removing the Signature element from the main tree to compute a new 
+        '''Removing the Signature element from the main tree to compute a new
         digest value with the actual content of the XML.'''
         signature_tree.getparent().remove(signature_tree)
         signed_info = _generate_signed_info(etree.tostring(tree))
-        
+
         '''Constructing the key from the Modulus and Exponent stored in the
-        Signature/KeyInfo/KeyValue/RSAKeyValue element'''                                 
+        Signature/KeyInfo/KeyValue/RSAKeyValue element'''
         from Crypto.PublicKey import RSA
         modulus = signature_tree.xpath(KEY_INFO_PATH + '/xmldsig:Modulus',
                                        namespaces=NAMESPACES)[0].text
         exponent = signature_tree.xpath(KEY_INFO_PATH + '/xmldsig:Exponent',
-                                        namespaces=NAMESPACES)[0].text        
+                                        namespaces=NAMESPACES)[0].text
         public_key = RSA.construct((b64d(modulus), b64d(exponent)))
-    
+
         '''Isolating the signature value to verify'''
         signature = signature_tree.xpath('xmldsig:SignatureValue',
                                          namespaces=NAMESPACES)[0].text
     except IndexError:
-        raise RuntimeError('XML does not contain a properly formatted ' \
+        import traceback
+        import sys
+        traceback.print_exc(file=sys.stdout)
+        raise RuntimeError('XML does not contain a properly formatted '
                            ' Signature element')
-    
+
     verifier = PKCS1_v1_5.PKCS115_SigScheme(public_key)
     return verifier.verify(SHA.new(c14n(signed_info)),
                            binascii.a2b_base64(signature))
 
 
-def _generate_key_info_xml_rsa(modulus, exponent):
+def _generate_key_info_xml_rsa(modulus, exponent, cert):
     '''
     Return <KeyInfo> xml bytestring using raw public RSA key.
     @param modulus: str of bytes
@@ -136,7 +136,8 @@ def _generate_key_info_xml_rsa(modulus, exponent):
     @return str of bytestring xml
     '''
     return PTN_KEY_INFO_RSA_KEY % {'modulus': b64e(modulus),
-                                   'exponent': b64e(exponent)}
+                                   'exponent': b64e(exponent),
+                                   'x509': cert}
 
 
 def _generate_signed_info(xml):
