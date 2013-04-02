cf72edfb8e82dcd6157001f55c25517b60365186
Alexander Schüssler <alexander.schussler@klarna.com>
2013-04-02 13:42:49 +0200

    Slimming down the setup

--- setup.py
+++ setup.py
@@ -4,17 +4,15 @@ try:
 except ImportError:
     from distutils.core import setup
 
-setup(
-    name                = 'pyxmli',
-    packages            = ['pyxmli'],
-    version             = open('VERSION').read(),
-    author              = u'Greendizer',
-    author_email        = 'developers@xmli.com',
-    install_requires    = ['pycrypto >= 2.6',
-                           'lxml >= 2.3'],
-    url                 = 'http://github.com/Greendizer/PyXMLi',
-    license             = open('LICENCE').read(),
-    description         = 'Create XMLi 2.0 compliant invoices in Python',
-    long_description    = open('README').read(),
-    zip_safe            = True,
-)
+setup(name='simplexmldsig',
+      packages=['simplexmldsig'],
+      version='0.1',
+      author=u'Greendizer',
+      author_email='developers@xmli.com',
+      install_requires=['pycrypto >= 2.6',
+                        'lxml >= 2.3'],
+      url='http://github.com/alx242/simplexmldsig',
+      license=open('LICENCE').read(),
+      description='Sign xml documents with a xmldsig',
+      zip_safe=True,
+      )
